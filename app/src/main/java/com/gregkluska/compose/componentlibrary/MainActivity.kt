0f7a45977ae78fc3e73e5510663d09cad6acfbd5
gregkluska <kluskagreg@gmail.com>
2022-01-23 16:11:01 +0000

    Migrate gradle to kts

--- app/src/main/java/com/gregkluska/compose/componentlibrary/MainActivity.kt
+++ app/src/main/java/com/gregkluska/compose/componentlibrary/MainActivity.kt
@@ -3,36 +3,12 @@ package com.gregkluska.compose.componentlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.gregkluska.compose.componentlibrary.ui.theme.ComponentLibraryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComponentLibraryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComponentLibraryTheme {
-        Greeting("Android")
     }
 }
